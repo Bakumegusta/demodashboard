da163dbbeb185adf19cf1fec48ef8cc3ae464728
Bakumegusta <bakumegusta@gmail.com>
2020-06-20 00:17:24 +0530

    push

--- js/main.js
+++ js/main.js
@@ -47,3 +47,97 @@
   });
 
 })(jQuery); // End of use strict
+
+getchat();
+async function getchat(){
+    const response = await fetch('data.json');
+    const data = await response.json();
+    console.log(data);
+    data.forEach(function(data){
+      console.log(data.answer);
+      var tr = document.createElement('tr');
+      var chatparent = document.querySelector('#chatparent');
+  
+      tr.innerHTML = `
+                    <td>${data.date}</td>  
+                    <td>${data.name}</td>
+                    <td>${data.email}</td>
+                    <td>${data.query}</td>
+                    <td>${data.answer}</td>
+                    <td>${data.sessionID}</td>                 
+                    <td ><button class ='session option' value ="${data.sessionID}" onclick="conversation(this.value)"> view convo</button></td>  
+                    `;
+                    chatparent.append(tr);
+    });
+    $('#dataTable').DataTable({
+      "bInfo" : false,
+      "order": [[ 0, "desc" ]]
+      // columnDefs: [{
+      //     width: '10%',
+      //     targets: 0
+      // }],
+    });
+
+}
+
+// conversation code
+async function conversation(value){
+  const response = await fetch('data.json');
+  const data = await response.json();
+  function sortByProperty(property){  
+    return function(a,b){  
+       if(a[property] > b[property])  
+          return 1;  
+       else if(a[property] < b[property])  
+          return -1;  
+   
+       return 0;  
+    }  
+ }
+  // console.log(value);
+
+  const convo = data.filter(function(convo){
+    if(convo.sessionID == value ){
+      return true;
+    }
+  });
+  convo.sort(sortByProperty('id'));
+  // console.log(convo);
+  convo.forEach(function(data){
+    // console.log(data);
+    
+    var convoparent = document.querySelector('#convoparent');
+    var answer = document.createElement('li');
+    var query = document.createElement('li');
+    answer.setAttribute('class',"in");
+    query.setAttribute('class',"out");
+                  query.innerHTML = `
+                  <div class="chat-body">
+                  <div class="chat-message">
+                    <p>${data.query}</p>
+                  </div>
+                </div>
+        
+                      `;
+                      answer.innerHTML = `
+                      <div class="chat-body">
+                      <div class="chat-message">
+                        <p>${data.answer}</p>
+                      </div>
+                    </div>
+            
+                          `;
+
+                      convoparent.append(query);
+                      convoparent.append(answer);
+
+                      $('#chatpop').show();
+                      $('.shut').on('click', function(){
+                        $('#chatpop').hide();
+                        $('.in').remove();
+                        $('.out').remove();                  
+                      })
+    
+  })
+
+}
